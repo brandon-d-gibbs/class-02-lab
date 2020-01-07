4a8259cab2fd03bd1ea2c30f9c42b5ec0202c335
Morgan <heinemann.morgan@gmail.com>
2020-01-07 14:01:06 -0800

    initial files created and index populated with biography

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// html page with CSS and JS
+// short biography <done>
+// 5 yes/no questions
+// use .toUpperCase() or .toLowerCase()
+/* console.log() for if the answer is correct or not
+replace with alerts at the end */
+
+// ask user for name via prompt()
+var user = prompt("What is your name?")
+
+// custom greeting for coming to site
+// display back in final message
