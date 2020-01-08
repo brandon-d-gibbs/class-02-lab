0a03b34227c53966b9255f08b38847b7dcf108fb
Morgan <heinemann.morgan@gmail.com>
2020-01-08 15:09:53 -0800

    changed first 5 questions to an array, commented out the if statements

--- js/app.js
+++ js/app.js
@@ -14,6 +14,31 @@ var user = prompt('What is your name?');
 alert('Hi ' + user + '! Welcome to the Morgan Quiz! I\'ve gota  few questions for you:');
 
 
+var questionArray = ['Morgan\'s had a dog named \'Ramen\': Y/N?',
+'Morgan only attended 4 schools in Bellingham: Y/N',
+'Morgan lived in Japan: Y/N',
+'Morgan has worked in software before: Y/N',
+'Morgan hates all things related to dragons and the dungeons they inhabit: Y/N'];
+
+var simpleAnswerArray = ['n', 'n', 'y', 'y', 'n'];
+
+var answerCount = 0;
+
+var testAnswer = '';
+if(questionArray.length !== simpleAnswerArray.length){
+  alert('MISMATCHED Q/A LENGTH');
+}
+for(let y = 0; y < questionArray.length; y++){
+  testAnswer = prompt(questionArray[y]);
+  if(testAnswer.charAt(0).toLowerCase() === simpleAnswerArray[y]){
+    alert('Correct!');
+    answerCount++;
+  }else{
+    alert('That is incorrect.');
+  }
+}
+
+/*
 // 5 yes/no questions <done>
 var dogName = prompt('Morgan\'s had a dog named \'Ramen\': Y/N?');
 var schoolCount = prompt('Morgan only attended 4 schools in Bellingham: Y/N');
@@ -74,7 +99,7 @@ if(tabletop.toLowerCase() === 'n' || tabletop.toLowerCase() === 'no'){
 }else{
   alert('I\'m only capable of understanding Yes/No for an answer regarding Morgan\'s obsession.');
 }
-
+*/
 
 //generates a random number to guess
 var numMin = 1;
@@ -83,7 +108,7 @@ var targetNumber = Math.floor(Math.random() * (numMax - numMin + 1) + numMin);
 var numberGuess = '';
 var guessNumberAttempts = 4;
 
-questionCount++; //question 6
+//questionCount++; //question 6
 for (let x = 0; x < guessNumberAttempts; x++){
   numberGuess = prompt('I\' thinking of a number between ' + numMin + ' and ' + numMax +'. What is your guess?');
   //this needs to check for only equal as the input may be caught as string
@@ -104,7 +129,7 @@ for (let x = 0; x < guessNumberAttempts; x++){
 }
 
 
-questionCount++; //question7
+//questionCount++; //question7
 //makes a normalized answer array based on answer array
 var answerArray = ['Los Angeles', 'Fukuyama', 'Bellingham', 'Seattle', 'Pullman'];
 var normalizedAnswerArray = [];
@@ -142,4 +167,4 @@ for (var x = 0; x < guessAnswerAttempts; x++) {
 } //end for
 
 // display back in final message
-alert('You got ' + answerCount + ' questions correct out of ' + questionCount + '. See Morgan\'s bio for answers and more!');
+alert('You got ' + answerCount + ' questions correct out of ' + (answerArray.length + 2) + '. See Morgan\'s bio for answers and more!');
